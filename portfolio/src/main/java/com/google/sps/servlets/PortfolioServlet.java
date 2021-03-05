e262c269961237cacaeaa0488d8c384f792ae179
Minolta <mndlovu@brynmawr.edu>
2021-03-05 00:49:14 +0000

    create a function that calls fetch() when you click a button and adds a response to the webpage

--- portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/main")
+@WebServlet("/message")
 public class PortfolioServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse 
     response) throws IOException{
-           response.getWriter().print("Hello world!");
+           response.setContentType("text/html;");
+           response.getWriter().print("Scroll down to learn more!");
     }
 }
