a1b00dddf1f8bfdfe9262bd87eb7c5a30e3ea24a
Minolta <mndlovu@brynmawr.edu>
2021-03-09 00:28:14 +0000

    make changes from feedback received

--- portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
@@ -1,11 +1,9 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.io.IOException;
-import java.util.ArrayList;
-import com.google.gson.Gson;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,23 +12,16 @@ public class PortfolioServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse 
     response) throws IOException{
-           /*response.setContentType("text/html;");
-           response.getWriter().print("Scroll down to learn more!");*/
            
            //Array that contains hard-coded strings 
-           String facts[] = {"I am from Bulawayo in Zimbabwe!"," I am the last born child of 13 children!",
+           String facts[] = {"I am from Bulawayo in Zimbabwe!", " I am the last born child of 13 children!", 
            " My name is the name of a camera!" };
-           
-           String jsonResponse = convertToJsonUsingGson(facts);
+
+           Gson gson = new Gson();
+           String jsonResponse = gson.toJson(facts);
 
            response.setContentType("application/json;");
            response.getWriter().print(jsonResponse);
     }
-
-    private String convertToJsonUsingGson(String[] stringArray) {
-    Gson gson = new Gson();
-    String json = gson.toJson(stringArray);
-    return json;
-  }
     
 }
